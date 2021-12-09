f5446a2e9512f17b154b1eef1151754da624af17
Adi Fatkhurozi <ady.fatkhurozi@gmail.com>
2021-12-09 19:27:34 +0700

    fix bug

--- next.config.js
+++ next.config.js
@@ -5,9 +5,7 @@ const config = {
   reactStrictMode: true,
   pwa: {
     dest: "public",
-    disable: false,
     fallbacks: {
-      image: "/static/images/fallback.png",
       // document: '/other-offline',  // if you want to fallback to a custom page other than /_offline
       // font: '/static/font/fallback.woff2',
       // audio: ...,
